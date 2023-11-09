0ba8e57c5b2da8fe91a2b7a3dc0076d80c6f83e7
inspectredc <inspectredc@gmail.com>
2023-11-09 15:47:15 +0000

    struct knowledge from func_80969114_jp

--- src/overlays/actors/ovl_Ball/ac_ball.h
+++ src/overlays/actors/ovl_Ball/ac_ball.h
@@ -21,9 +21,10 @@ typedef void (*BallActionFunc)(struct Ball*, struct Game_Play*);
 typedef struct Ball {
     Actor actor;
     ClObjPipe collider; // 0x174
-    s8 unk_190[0x4C];
+    s8 unk_190[0x40];
+    xyz_t unk_1D0;
     s32 unk_1DC;
-    s8 unk_1E0[0x4];
+    void* unk_1E0;
     void* unk_1E4;
     s8 unk_1E8[0x4];
     f32 unk_1EC;
