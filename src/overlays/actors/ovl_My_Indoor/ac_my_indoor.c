6ced3592c0044d29c39b48d6c4e8e195cbe9d681
inspectredc <inspectredc@gmail.com>
2023-11-20 11:20:55 +0000

    func_80951BC4_jp and func_80951CDC_jp

--- src/overlays/actors/ovl_My_Indoor/ac_my_indoor.c
+++ src/overlays/actors/ovl_My_Indoor/ac_my_indoor.c
@@ -33,9 +33,41 @@ s32 func_80951A70_jp(s32 arg0) {
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_My_Indoor/ac_my_indoor/func_80951A9C_jp.s")
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_My_Indoor/ac_my_indoor/func_80951BC4_jp.s")
+extern UNK_TYPE D_17A1000;
+#define UNK_SIZE_1 0x2020
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_My_Indoor/ac_my_indoor/func_80951CDC_jp.s")
+void func_80951BC4_jp(My_Indoor* this, s16 arg0, s16 arg1) {
+    s32 i;
+    void **vramPtr;
+    vramPtr = this->unk_180;
+    if (arg1 == 2) {
+        for (i = 0; i < 2; i++) {
+            if (vramPtr[i] != NULL) {
+                DmaMgr_RequestSyncDebug(vramPtr[i], (uintptr_t)&D_17A1000 + (arg0 * UNK_SIZE_1), UNK_SIZE_1, "../ac_my_indoor.c", 0x16C);
+            }
+        } 
+    } else if (vramPtr[arg1] != NULL) {
+        DmaMgr_RequestSyncDebug(vramPtr[arg1], (uintptr_t)&D_17A1000 + (arg0 * UNK_SIZE_1), UNK_SIZE_1, "../ac_my_indoor.c", 0x174);
+    }
+}
+
+extern UNK_TYPE D_182A000;
+#define UNK_SIZE_2 0x1020
+
+void func_80951CDC_jp(My_Indoor* this, s16 arg1, s16 arg2) {
+    s32 i;
+    void **vramPtr;
+    vramPtr = this->unk_188;
+    if (arg2 == 2) {
+        for (i = 0; i < 2; i++) {
+            if (vramPtr[i] != NULL) {
+                DmaMgr_RequestSyncDebug(vramPtr[i], (uintptr_t)&D_182A000 + (arg1 * UNK_SIZE_2), UNK_SIZE_2, "../ac_my_indoor.c", 0x194);
+            }
+        }
+    } else if (vramPtr[arg2] != NULL) {
+        DmaMgr_RequestSyncDebug(vramPtr[arg2], (uintptr_t)&D_182A000 + (arg1 * UNK_SIZE_2), UNK_SIZE_2, "../ac_my_indoor.c", 0x1A0);
+    }
+}
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_My_Indoor/ac_my_indoor/func_80951DF4_jp.s")
 
